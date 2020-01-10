f07948ed837b0310deacf4f4f85cec6f669ab2c2
mich132 <mikael0setiawan@gmail.com>
2020-01-10 20:42:33 +0700

    Automation selenium python

---
+++ test_simple.py
@@ -0,0 +1,30 @@
+from selenium.webdriver import Chrome
+def test_verify_registration():
+    driver = Chrome()
+    driver.get("https://thetestingworld.com/testings")
+    # driver.implicitly_wait(10)
+    driver.maximize_window()
+
+def test_verify2_registration():
+    driver = Chrome()
+    driver.get("https://thetestingworld.com/testings")
+    # driver.implicitly_wait(10)
+    driver.maximize_window()
+
+def test_verify3_registration():
+    driver = Chrome()
+    driver.get("https://thetestingworld.com/testings")
+    # driver.implicitly_wait(10)
+    driver.maximize_window()
+
+def test_verify4_registration():
+    driver = Chrome()
+    driver.get("https://thetestingworld.com/testings")
+    # driver.implicitly_wait(10)
+    driver.maximize_window()
+
+def test_verify5_registration():
+    driver = Chrome()
+    driver.get("https://thetestingworld.com/testings")
+    # driver.implicitly_wait(10)
+    driver.maximize_window()
---
+++ test_simple2.py
@@ -0,0 +1,30 @@
+from selenium.webdriver import Chrome
+def test_verify6_registration():
+    driver = Chrome()
+    driver.get("https://thetestingworld.com/testings")
+    # driver.implicitly_wait(10)
+    driver.maximize_window()
+
+def test_verify7_registration():
+    driver = Chrome()
+    driver.get("https://thetestingworld.com/testings")
+    # driver.implicitly_wait(10)
+    driver.maximize_window()
+
+def test_verify8_registration():
+    driver = Chrome()
+    driver.get("https://thetestingworld.com/testings")
+    # driver.implicitly_wait(10)
+    driver.maximize_window()
+
+def test_verify9_registration():
+    driver = Chrome()
+    driver.get("https://thetestingworld.com/testings")
+    # driver.implicitly_wait(10)
+    driver.maximize_window()
+
+def test_verify10_registration():
+    driver = Chrome()
+    driver.get("https://thetestingworld.com/testings")
+    # driver.implicitly_wait(10)
+    driver.maximize_window()
